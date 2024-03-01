be5f1d0169d8c35b69419c26242066d3597c759b
Davi PC <davi.frrs@gmail.com>
2024-03-01 08:05:35 -0300

    initial api setup + auth

--- api_service/config/config.py
+++ api_service/config/config.py
@@ -1,15 +1,20 @@
-from pydantic_settings import BaseSettings
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+DOTENV = os.path.join(os.path.dirname(__file__), "env")
 
 
 class Settings(BaseSettings):
     API_VERSION: str = "v1"
     API_V1_STR: str = f"/api/{API_VERSION}"
     PROJECT_NAME: str
-    USERNAME: str
-    PASSWORD: str
+    AUTH_USERNAME: str
+    AUTH_PASSWORD: str
 
+    # model_config = SettingsConfigDict(env_file='../.env', env_file_encoding="utf-8")
+    
     class Config:
-        env_file = "./api_service/.env"
+        env_file = ".env"
         env_file_encoding = "utf-8"
 
 
--- api_service/main.py
+++ api_service/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
-from api_service.config.config import settings
-from api_service.api.v1.api import api_router as api_router_v1
+from uu import encode
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from config.config import settings
+from api.v1.api import api_router as api_router_v1
+from base64 import b64decode
+import uvicorn
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -8,4 +12,32 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
 
+@app.middleware("http")
+async def check_user_permissions(request: Request, call_next):
+    try:
+        auth = request.headers["Authorization"]
+        token = auth.split(' ')[1]  
+        userData = decodeBase64(token)
+
+        if (userData[0] != settings.AUTH_USERNAME or userData[1] != settings.AUTH_PASSWORD):
+            raise Exception("Invalid authentication data!") 
+
+        response = await call_next(request)
+        
+        return response
+    except Exception:
+        return JSONResponse("Invalid authentication data!", status_code=401)
+    
+
+def decodeBase64(encodedToken: str):
+    ascii_encode = encodedToken.encode("ascii")
+    base64_decoded = b64decode(ascii_encode)
+    ascii_decoded = base64_decoded.decode("ascii")
+
+    return ascii_decoded.split(':')
+    
+
 app.include_router(api_router_v1, prefix=settings.API_V1_STR)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=7777)
